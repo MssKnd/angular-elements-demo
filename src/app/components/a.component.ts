ff0a6b1ba6a95338bb8fd6e088e42c1d307271ea
Masashi Kondo <m-kondo@safie.jp>
2020-09-25 07:08:06 +0900

    add Lazy loading component

--- src/app/components/a.component.ts
+++ src/app/components/a.component.ts
@@ -1,6 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewContainerRef } from '@angular/core';
 
 @Component({
-  template: 'Component_A',
+  template: `
+    <input type="button" value="button" (click)="onClick()" />
+  `,
 })
-export class AComponent {}
+export class AComponent {
+  constructor(
+    public viewContainerRef: ViewContainerRef,
+    private readonly componentFactoryResolver: ComponentFactoryResolver
+  ) {}
+  async onClick() {
+    const { LazyComponent } = await import('./lazy.component');
+    const component = this.componentFactoryResolver.resolveComponentFactory(
+      LazyComponent
+    );
+    this.viewContainerRef.createComponent(component);
+  }
+}
---
+++ src/app/components/lazy.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: 'Component_Lazy',
+})
+export class LazyComponent {}
