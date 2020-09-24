ac777cf0696de4b997ddd06c31ba9eaa8ad7b228
Masashi Kondo <m-kondo@safie.jp>
2020-09-24 23:14:00 +0900

    minimum

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
+import { AComponent } from './components/a.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {}
+  ngDoBootstrap() {
+    const AComponentElement = createCustomElement(
+      AComponent,
+      { injector: this.injector }
+    );
+    customElements.define('a-component', AComponentElement);
+  }
+}
---
+++ src/app/components/a.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: 'Component_A',
+})
+export class AComponent {}
