cbb4fbf7436a1a85bef700c3c35b6566633c12fb
Masashi Kondo <m-kondo@safie.jp>
2020-10-11 18:47:44 +0900

    demo fixed

--- src/app/components/a.component.ts
+++ src/app/components/a.component.ts
@@ -2,5 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   template: '<span class="light">Light Component</span>',
+  styles: ['span { padding: 50px; line-height: 100px; }'],
 })
 export class AComponent {}
--- src/app/components/b.component.ts
+++ src/app/components/b.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   template: '<span class="shadow">Shadow Component</span>',
+  styles: ['span { padding: 50px; line-height: 100px; }'],
   encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class BComponent {}
