e69227a0bcb01022990857252603ea91e477e0f8
Masashi Kondo <m-kondo@safie.jp>
2020-10-17 03:26:35 +0900

    fix

--- src/app/components/a.component.ts
+++ src/app/components/a.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   template: '<span class="light">Light Component</span>',
-  styles: ['span { padding: 50px; line-height: 100px; }'],
+  styles: ['span { padding: 40px 20px; line-height: 100px; }'],
 })
 export class AComponent {}
--- src/app/components/b.component.ts
+++ src/app/components/b.component.ts
@@ -2,7 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   template: '<span class="shadow">Shadow Component</span>',
-  styles: ['span { padding: 50px; line-height: 100px; }'],
+  styles: ['span { padding: 40px 20px; line-height: 100px; }'],
   encapsulation: ViewEncapsulation.ShadowDom,
 })
 export class BComponent {}
